8ff9375e992fb4bf8ba3673d4a2910623b0228ea
supershadoe <supershadoe@proton.me>
2024-07-03 22:33:03 +0530

    ⬆ Update daisyui ver as well

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 
 <svelte:head>
     <title>shadoe</title>
+    <meta name="description" content="supershadoe creates stuff with clean and maintainable code, a good looking UI with decent UX and a short but unique and powerful list of features." />
 </svelte:head>
 
 <article class="prose ml-8 mt-8">
