d4440d2650ea27262b48246ac96db42e2fa88ed5
supershadoe <supershadoe@proton.me>
2025-03-27 23:19:02 +0530

    eslint stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
 import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
+    astro.configs.recommended,
+    astro.configs['flat/jsx-a11y-recommended'],
     prettier,
     {
         ignores: ['.astro/', 'dist/'],
